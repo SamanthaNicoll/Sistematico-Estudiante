f28f6d536585d07c696cd19f0ef316a110ac1200
Samantha Nicoll <85895313+SamanthaNicoll@users.noreply.github.com>
2022-05-19 11:50:34 -0600

    Add files via upload

---
+++ Sistematico.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Sistematico.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ Sistematico.Domain/Entities/Estudiante.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistematico.Domain.Entities
+{
+    public partial class Estudiante
+    {
+        public Estudiante()
+        {
+
+        }
+        public int Id { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Carnet { get; set; }
+        public string Phone { get; set; }
+        public string Direccion { get; set; }
+        public string Correo { get; set; }
+        public int Matematica { get; set; }
+        public int Contabilidad { get; set; }
+        public int Programacion { get; set; }
+        public int Estadistica { get; set; }
+
+
+    }
+}
